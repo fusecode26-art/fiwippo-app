477d5f5e5d58b6f57b927c46f17c503ef90caa36
jihad <md.jihad.info2006@gmail.com>
2026-01-01 18:14:57 +0600

    last uni commit

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -6,24 +6,24 @@ class AppTheme {
   AppTheme._();
 
   static ThemeData lightTheme = ThemeData(
-    // useMaterial3: true,
-    // textTheme: GoogleFonts.outfitTextTheme().apply(
-    //   bodyColor: Colors.black87,
-    //   displayColor: Colors.black87,
-    // ),
+    useMaterial3: false,
+    textTheme: GoogleFonts.outfitTextTheme().apply(
+      bodyColor: Colors.black87,
+      displayColor: Colors.black87,
+    ),
     elevatedButtonTheme: ElevatedButtonThemeData(
       style: ElevatedButton.styleFrom(
         minimumSize: Size(double.infinity, 50),
         backgroundColor: Color(0xff6200EE),
         foregroundColor: Colors.white,
         textStyle: GoogleFonts.outfit(
-          fontSize: 16.sp,
+          fontSize: 16,
           fontWeight: FontWeight.w600,
         ),
         shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(12.r),
+          borderRadius: BorderRadius.circular(12),
         ),
-        padding: EdgeInsets.symmetric(vertical: 10.h, horizontal: 20.w),
+        padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
         elevation: 3,
       ),
     ),
@@ -33,43 +33,43 @@ class AppTheme {
         side: BorderSide(color: Colors.blueAccent, width: 1.5),
         foregroundColor: Colors.blueAccent,
         textStyle: GoogleFonts.outfit(
-          fontSize: 16.sp,
+          fontSize: 16,
           fontWeight: FontWeight.w600,
         ),
         shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(25.r),
+          borderRadius: BorderRadius.circular(25),
         ),
-        padding: EdgeInsets.symmetric(vertical: 14.h, horizontal: 20.w),
+        padding: EdgeInsets.symmetric(vertical: 14, horizontal: 20),
       ),
     ),
 
     inputDecorationTheme: InputDecorationTheme(
       filled: true,
       fillColor: Color(0xffF7F7F7),
-      contentPadding: EdgeInsets.symmetric(vertical: 12.h, horizontal: 16.w),
+      contentPadding: EdgeInsets.symmetric(vertical: 12, horizontal: 16),
       border: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(12.r),
+        borderRadius: BorderRadius.circular(12),
         borderSide: BorderSide.none,
       ),
       enabledBorder: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(12.r),
+        borderRadius: BorderRadius.circular(12),
         borderSide: BorderSide.none,
       ),
       focusedBorder: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(12.r),
+        borderRadius: BorderRadius.circular(12),
         borderSide: BorderSide.none,
       ),
       errorBorder: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(12.r),
+        borderRadius: BorderRadius.circular(12),
         borderSide: BorderSide(width: 1.5, color: Colors.red),
       ),
       hintStyle: GoogleFonts.outfit(
-        fontSize: 14.sp,
+        fontSize: 14,
         fontWeight: FontWeight.normal,
         color: Colors.grey.shade500,
       ),
       labelStyle: GoogleFonts.outfit(
-        fontSize: 14.sp,
+        fontSize: 14,
         fontWeight: FontWeight.w500,
         color: Colors.grey.shade700,
       ),
